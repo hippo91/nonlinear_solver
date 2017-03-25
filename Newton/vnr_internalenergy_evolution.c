9513d87a4ff5d4f6b21b680c72e57ebd289bc8ec
guillaume2 <guillaume.peillex@gmail.col>
2017-03-25 10:45:37 +0100

    Use of specific allocation for OpenMP

--- Newton/vnr_internalenergy_evolution.c
+++ Newton/vnr_internalenergy_evolution.c
@@ -3,20 +3,27 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "utils.h"
 
 void internal_energy_evolution_VNR(void *variables, double *newton_var, size_t size_of_pb, double *func, double *dfunc)
 {
     VnrVariables_t *vars = (VnrVariables_t*) variables;
-    double *pression = calloc(size_of_pb, sizeof(double));
-    double *dpsurde = calloc(size_of_pb, sizeof(double));
-    double *dummy = calloc(size_of_pb, sizeof(double));
-    double *specific_volume = calloc(size_of_pb, sizeof(double));
+    double *pression = NULL;
+    double *dpsurde = NULL;
+    double *dummy = NULL;
+    double *specific_volume = NULL;
+    allocVecForOMP(size_of_pb, 0., &pression);
+    allocVecForOMP(size_of_pb, 0., &dpsurde);
+    allocVecForOMP(size_of_pb, 0., &dummy);
+    allocVecForOMP(size_of_pb, 0., &specific_volume);
+#pragma omp parallel for
     for (int i=0; i<size_of_pb; ++i) {
         specific_volume[i] = 1. / vars->density_new[i];
     }
     // Appel de l'eos
     vars->miegruneisen->solve(vars->miegruneisen, size_of_pb, specific_volume, newton_var, pression, dpsurde, dummy);
     double delta_v = 0.;
+#pragma omp parallel for
     for (int i=0; i<size_of_pb; ++i) {
         delta_v = 1. / vars->density_new[i] - 1. / vars->density_old[i];
         // Fonction à annuler
