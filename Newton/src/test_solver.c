52b60d15fc1cbf83934e66315180d4d549688b8a
hippo91 <guillaume.peillex@gmail.com>
2020-04-10 14:52:37 +0200

    Finalizes the test of the solver. Two values are said to be equal if their relative difference is below 1.e-15.

--- Newton/src/test_solver.c
+++ Newton/src/test_solver.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,7 +7,8 @@
 #include "launch_vnr_resolution.h"
 
 /**
- * @brief Check if an array is filled with a unique uniform value 
+ * @brief Check if an array is filled with a unique uniform value.
+ * 		  Two values are said to be equal if their relative difference is below 1.e-15.
  * 
  * @param array : array to check
  * @param size : size of the array
@@ -19,7 +21,7 @@
 bool is_uniform_array_value(const double* const array, const size_t size, const double value, int* unvalid_index) {
 	size_t inv_ind = 0;
 	for (size_t i = 0; i < size; ++i) {
-		if (array[i] != value) {
+		if (fabs(array[i] - value) / fabs(value) > 1.e-15) {
 			unvalid_index[inv_ind++] = i;
 		}
 	};
@@ -35,7 +37,7 @@ bool is_uniform_array_value(const double* const array, const size_t size, const 
  * @param expected : expected value that should be found a the arr[index] place
  */
 void print_array_index_error(const char* array_name, const size_t index, const double* const array, const double expected) {
-	fprintf(stderr, "%s[%zu] = %15.9g instead of %15.9g\n", array_name, index, array[index], expected);
+	fprintf(stderr, "%s[%zu] = %22.16g instead of %22.16g\n", array_name, index, array[index], expected);
 }
 
 /**
@@ -92,15 +94,14 @@ int main() {
 
 	launch_vnr_resolution(old_density, new_density, pressure, internal_energy, pb_size, solution, new_pressure, new_cson);
 
-	old_density[3] = 8230.0001;
-	old_density[5] = 8230.02;
-	old_density[7] = 8230.0003;
-	new_density[2] = 9204.;
-	new_density[6] = 9500.011;
-
 	bool success = true;
 	if (!check_uniform_array_value(old_density, pb_size, "old_density", 8230.)) success = false;
 	if (!check_uniform_array_value(new_density, pb_size, "new_density", 9500.)) success = false;
+	if (!check_uniform_array_value(pressure, pb_size, "pressure", 10.e+09)) success = false;
+	if (!check_uniform_array_value(internal_energy, pb_size, "internal_energy", 1.325e+04)) success = false;
+	if (!check_uniform_array_value(solution, pb_size, "solution", 200765.8953965593)) success = false;
+	if (!check_uniform_array_value(new_pressure, pb_size, "new_pressure", 13088079183.59054)) success = false;
+	if (!check_uniform_array_value(new_cson, pb_size, "new_cson", 4503.84710590959)) success = false;
 
 	free(solution);
 	free(old_density);
