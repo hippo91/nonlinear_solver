b511cafa6beeb92a4cc87643cca75aa40cbcf52a
hippo91 <guillaume.peillex@gmail.com>
2020-04-10 18:39:21 +0200

    Rewrites the ensure_positivity_incrementation method so that the computed increment doesn't change sign of the x vector

--- Newton/src/incrementation/incrementations_methods.c
+++ Newton/src/incrementation/incrementations_methods.c
@@ -15,15 +15,14 @@ void damped_incrementation(__attribute__((unused)) double *x_k, double *func, do
 }
 
 void ensure_positivity_incrementation(double *x_k, double *func, double *dfunc, size_t size_of_pb, double *vector_of_increments) {
-	double damping_coeff = 0.5;
 	for (size_t i=0; i<size_of_pb; ++i) {
-		double min_authorized = damping_coeff * x_k[i];
+		double min_authorized = 0.;
 		double optimal_value = - func[i] / dfunc[i];
-		if (optimal_value < min_authorized) {
-			vector_of_increments[i] = min_authorized;
+		double target = x_k[i] + optimal_value;
+		if (target * x_k[i] < 0) {
+			vector_of_increments[i] = (min_authorized - x_k[i]) * 0.5;
 		} else {
 			vector_of_increments[i] = optimal_value;
 		}
-
 	}
 }
