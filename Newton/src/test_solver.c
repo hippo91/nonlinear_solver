96548ccfc8068fe5384719ae57f3f224f518b266
hippo91 <guillaume.peillex@gmail.com>
2020-04-10 14:34:11 +0200

    Add doxygen comments. The return value of the check_uniform_array_value is now a boolean

--- Newton/src/test_solver.c
+++ Newton/src/test_solver.c
@@ -5,6 +5,17 @@
 #include "utils.h"
 #include "launch_vnr_resolution.h"
 
+/**
+ * @brief Check if an array is filled with a unique uniform value 
+ * 
+ * @param array : array to check
+ * @param size : size of the array
+ * @param value : value expected to be at every index of the array
+ * @param unvalid_index : an array, which size is the same as the array to check, and will hold the indices
+ * 						  where the value found in the array is different from the one expected
+ * @return true : if all the indices of the array owns the same value
+ * @return false : otherwise
+ */
 bool is_uniform_array_value(const double* const array, const size_t size, const double value, int* unvalid_index) {
 	size_t inv_ind = 0;
 	for (size_t i = 0; i < size; ++i) {
@@ -15,28 +26,53 @@ bool is_uniform_array_value(const double* const array, const size_t size, const 
 	return inv_ind == 0;
 }
 
-void print_array_index_error(const char* array_name, const size_t index, const double obtained, const double expected) {
-	fprintf(stderr, "%s[%zu] = %15.9g instead of %15.9g\n", array_name, index, obtained, expected);
+/**
+ * @brief Print to the standard error a formatted error message
+ * 
+ * @param array_name : name of the array 
+ * @param index : index of the array from which the value will be printed
+ * @param array : array 
+ * @param expected : expected value that should be found a the arr[index] place
+ */
+void print_array_index_error(const char* array_name, const size_t index, const double* const array, const double expected) {
+	fprintf(stderr, "%s[%zu] = %15.9g instead of %15.9g\n", array_name, index, array[index], expected);
 }
 
-
-void check_uniform_array_value(const double * const arr, const size_t size,
+/**
+ * @brief Check if an array is filled with a unique uniform value and prints an error message
+ * 		  if not.
+ * 
+ * @param arr : array to check
+ * @param size : size of the array
+ * @param arr_name : name of the array
+ * @param expected : value expected to fill the array
+ * @return true : if the array is uniformely filled with the expected value
+ * @return false : otherwise
+ */
+bool check_uniform_array_value(const double * const arr, const size_t size,
 							   const char* arr_name, const double expected) {
-	int invalid_input[size];
-	for (size_t i = 0; i < size; ++i) invalid_input[i] = -1;
+	int invalid_entries[size];
+	for (size_t i = 0; i < size; ++i) invalid_entries[i] = -1;
 
-	if (!is_uniform_array_value(arr, size, expected, invalid_input)) {
+	bool valid = is_uniform_array_value(arr, size, expected, invalid_entries);
+	if (!valid) {
 		fprintf(stderr, "Wrong values for %s!\n", arr_name);
 		size_t _index = 0;
-		while(_index < size && invalid_input[_index] != -1) {
-			print_array_index_error(arr_name, invalid_input[_index],
-					 				arr[invalid_input[_index]], expected);
+		while(_index < size && invalid_entries[_index] != -1) {
+			print_array_index_error(arr_name, invalid_entries[_index],
+					 				arr, expected);
 			_index++;
 		}
 	}
+
+	return valid;
 }
 
-
+/**
+ * @brief Launch the test of the nonlinear solver
+ * 
+ * @return int : success (0) or failure (1)
+ */
 int main() {
 	double* solution;
 	size_t pb_size = 10;
@@ -61,13 +97,17 @@ int main() {
 	old_density[7] = 8230.0003;
 	new_density[2] = 9204.;
 	new_density[6] = 9500.011;
-	check_uniform_array_value(old_density, pb_size, "old_density", 8230.);
-	check_uniform_array_value(new_density, pb_size, "new_density", 9500.);
+
+	bool success = true;
+	if (!check_uniform_array_value(old_density, pb_size, "old_density", 8230.)) success = false;
+	if (!check_uniform_array_value(new_density, pb_size, "new_density", 9500.)) success = false;
 
 	free(solution);
 	free(old_density);
 	free(new_density);
 	free(pressure);
 	free(internal_energy);
-	return (EXIT_SUCCESS);
+
+	if (!success) return (EXIT_FAILURE);
+	return EXIT_SUCCESS;
 }
