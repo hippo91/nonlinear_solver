473f449c6b5662f980994aff9237b539ef0942e9
hippo91 <guillaume.peillex@gmail.com>
2020-04-11 14:04:34 +0200

    Adds doxygen

--- Newton/src/criterions/stop_criterions.c
+++ Newton/src/criterions/stop_criterions.c
@@ -3,6 +3,18 @@
 #include <math.h>
 #include <omp.h>
 
+/**
+ * @brief Check the convergence of the function and update the value of x vector according to 
+ * 		  the incrementation vector.
+ * 		  The convergence is obtained if the value of the function is sufficiently close to zero
+ * 
+ * @param x_k : current Newton unknown vector
+ * @param delta_x_k : Newton's incrementation vector
+ * @param func : vector of the function's values
+ * @param size_of_pb : size of the problem (and of all arrays)
+ * @param x_k_pun : new Newton unknown vector
+ * @param has_converged : vector of boolean indicating the convergence 
+ */
 void relative_gap(double *x_k, double *delta_x_k, double *func, size_t size_of_pb, double *x_k_pun, bool *has_converged) {
 	double epsilon = 1.0e-08;
 	double precision = 1.0e-09;
