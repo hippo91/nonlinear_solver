e69dfa57bbe1e21e3455fee906ab8bcc13707f18
hippo91 <guillaume.peillex@gmail.com>
2020-04-12 10:53:41 +0200

    Reformat using clangd

--- Newton/src/eos/miegruneisen.h
+++ Newton/src/eos/miegruneisen.h
@@ -3,16 +3,19 @@
 typedef struct MieGruneisenParameters MieGruneisenParameters_t;
 
 struct MieGruneisenParameters {
-        double c_zero;
-        double s1;
-        double s2;
-        double s3;
-        double rho_zero;
-        double gamma_zero;
-        double coeff_b;
-        double e_zero;
-        void (*solve)(MieGruneisenParameters_t*, const int, const double*, const double*, double *, double *, double *);
+  double c_zero;
+  double s1;
+  double s2;
+  double s3;
+  double rho_zero;
+  double gamma_zero;
+  double coeff_b;
+  double e_zero;
+  void (*solve)(MieGruneisenParameters_t *, const int, const double *,
+                const double *, double *, double *, double *);
 };
 
-void solveVolumeEnergyVec(MieGruneisenParameters_t*, const int nb_cells, const double* specific_volume,
-                          const double* internal_energy, double *pressure, double *gamma_per_vol, double *c_son);
+void solveVolumeEnergyVec(MieGruneisenParameters_t *, const int nb_cells,
+                          const double *specific_volume,
+                          const double *internal_energy, double *pressure,
+                          double *gamma_per_vol, double *c_son);
--- Newton/src/launch_vnr_resolution/launch_vnr_resolution.c
+++ Newton/src/launch_vnr_resolution/launch_vnr_resolution.c
@@ -1,11 +1,11 @@
+#include "incrementations_methods.h"
+#include "newton.h"
+#include "stop_criterions.h"
+#include "vnr_internalenergy_evolution.h"
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
 #include <string.h>
-#include "vnr_internalenergy_evolution.h"
-#include "incrementations_methods.h"
-#include "stop_criterions.h"
-#include "newton.h"
 #ifndef MIEGRUNEISEN
 #include "miegruneisen.h"
 #define MIEGRUNEISEN
@@ -13,30 +13,34 @@
 #include "utils.h"
 //#define MEASURE_TIME
 
-void launch_vnr_resolution(double *old_density, double *new_density, double *pressure, double *internal_energy,
-                           int pb_size, double *solution, double *new_p, double *new_vson) {
+void launch_vnr_resolution(double *old_density, double *new_density,
+                           double *pressure, double *internal_energy,
+                           int pb_size, double *solution, double *new_p,
+                           double *new_vson) {
 #ifdef MEASURE_TIME
-	/*
-	 * POUR LA MESURE DU TEMPS
-	 */
-	double begin = 0.;
-	double end = 0.;
-	double delta = 0.;
+  /*
+   * POUR LA MESURE DU TEMPS
+   */
+  double begin = 0.;
+  double end = 0.;
+  double delta = 0.;
 #endif
-    /*
-     * DEFINITION DE L EOS
-     * */
-	double czero = 3940.0, S1 = 1.489, S2 = 0., S3 = 0., rhozero = 8930.0, grunzero = 2.02, b = 0.47, ezero = 0.;
-	MieGruneisenParameters_t MieGruneisenParams= {czero, S1, S2, S3, rhozero, grunzero, b, ezero, solveVolumeEnergyVec};
-	/*
-	 * DEFINITION DE LA FONCTION A RESOUDRE (EQUATION D EVOLUTION DE L ENERGIE
-	 * INTERNE DANS LE SCHEMA VNR)
-     * FIXATION DE LA METHODE D'INCREMENTATION ET DU CRITERE D'ARRET
-	 * */
-  double * l_old_density = NULL;
-  double * l_new_density = NULL;
-  double * l_pressure = NULL;
-  double * l_internal_energy = NULL;
+  /*
+   * DEFINITION DE L EOS
+   * */
+  double czero = 3940.0, S1 = 1.489, S2 = 0., S3 = 0., rhozero = 8930.0,
+         grunzero = 2.02, b = 0.47, ezero = 0.;
+  MieGruneisenParameters_t MieGruneisenParams = {
+      czero, S1, S2, S3, rhozero, grunzero, b, ezero, solveVolumeEnergyVec};
+  /*
+   * DEFINITION DE LA FONCTION A RESOUDRE (EQUATION D EVOLUTION DE L ENERGIE
+   * INTERNE DANS LE SCHEMA VNR)
+   * FIXATION DE LA METHODE D'INCREMENTATION ET DU CRITERE D'ARRET
+   * */
+  double *l_old_density = NULL;
+  double *l_new_density = NULL;
+  double *l_pressure = NULL;
+  double *l_internal_energy = NULL;
   allocVecForOMP(pb_size, 0., &l_old_density);
   allocVecForOMP(pb_size, 0., &l_new_density);
   allocVecForOMP(pb_size, 0., &l_pressure);
@@ -45,14 +49,16 @@ void launch_vnr_resolution(double *old_density, double *new_density, double *pre
   memmove(l_new_density, new_density, pb_size * sizeof(double));
   memmove(l_pressure, pressure, pb_size * sizeof(double));
   memmove(l_internal_energy, internal_energy, pb_size * sizeof(double));
-  VnrVariables_t VnrVars = {l_old_density, l_new_density, l_internal_energy, l_pressure, &MieGruneisenParams};
-	NewtonParameters_t TheNewton = {internal_energy_evolution_VNR, classical_incrementation, relative_gap};
+  VnrVariables_t VnrVars = {l_old_density, l_new_density, l_internal_energy,
+                            l_pressure, &MieGruneisenParams};
+  NewtonParameters_t TheNewton = {internal_energy_evolution_VNR,
+                                  classical_incrementation, relative_gap};
 #ifdef MEASURE_TIME
-	begin = omp_get_wtime();
+  begin = omp_get_wtime();
 #endif
-	solveNewton(&TheNewton, &VnrVars, internal_energy, pb_size, &solution);
-    // Appel de l'eos avec la solution du newton pour calculer la nouvelle
-    // pression et vitesse du son
+  solveNewton(&TheNewton, &VnrVars, internal_energy, pb_size, &solution);
+  // Appel de l'eos avec la solution du newton pour calculer la nouvelle
+  // pression et vitesse du son
   double *specific_volume = NULL;
   allocVecForOMP(pb_size, 0., &specific_volume);
   for (int i = 0; i < pb_size; ++i) {
@@ -60,7 +66,8 @@ void launch_vnr_resolution(double *old_density, double *new_density, double *pre
   }
   double *dummy = NULL;
   allocVecForOMP(pb_size, 0., &dummy);
-  VnrVars.miegruneisen->solve(VnrVars.miegruneisen, pb_size, specific_volume, solution, new_p, dummy, new_vson);
+  VnrVars.miegruneisen->solve(VnrVars.miegruneisen, pb_size, specific_volume,
+                              solution, new_p, dummy, new_vson);
   free(l_old_density);
   free(l_new_density);
   free(l_pressure);
@@ -68,8 +75,8 @@ void launch_vnr_resolution(double *old_density, double *new_density, double *pre
   free(specific_volume);
   free(dummy);
 #ifdef MEASURE_TIME
-	end = omp_get_wtime();
-	delta = end - begin;
-	printf("Temps consommé : %lf secondes\n", delta);
+  end = omp_get_wtime();
+  delta = end - begin;
+  printf("Temps consommé : %lf secondes\n", delta);
 #endif
 }
