25b7e591133c6f1a0541632efb89e240451bd0a7
hippo91 <guillaume.peillex@gmail.com>
2020-04-12 11:56:01 +0200

    Change the type of counter from int to size_t

--- Newton/src/functions/quadratic_function.c
+++ Newton/src/functions/quadratic_function.c
@@ -10,7 +10,7 @@
 void quadratic_function(__attribute__((unused)) void *empty, double *x, size_t size_of_pb, double *func,
                         double *dfunc)
 {
-    for (int i = 0; i < size_of_pb; ++i)
+    for (size_t i = 0; i < size_of_pb; ++i)
     {
         func[i] = x[i] * x[i] + 2. * x[i] - 9.0;
         dfunc[i] = 2 * x[i] + 2.;
