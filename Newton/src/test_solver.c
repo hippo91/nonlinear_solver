93025196fad1558ef13b77ef67abc7d217534e56
hippo91 <guillaume.peillex@gmail.com>
2020-04-22 15:39:23 +0200

    Takes into account the change in the build_array function (returns a pointer)

--- Newton/src/test_solver.c
+++ Newton/src/test_solver.c
@@ -17,57 +17,65 @@
 int main()
 {
     const size_t pb_size = 10;
-    s_array solution, old_density, old_specific_volume;
-    s_array new_density, new_specific_volume, pressure;
-    s_array internal_energy, new_pressure, new_cson;
 
-    build_array(pb_size, "old_density", &old_density);
-    build_array(pb_size, "old_specific_volume", &old_specific_volume);
-    build_array(pb_size, "new_density", &new_density);
-    build_array(pb_size, "new_specific_volume", &new_specific_volume);
-    build_array(pb_size, "pressure", &pressure);
-    build_array(pb_size, "internal_energy", &internal_energy);
-    build_array(pb_size, "solution", &solution);
-    build_array(pb_size, "new_pressure", &new_pressure);
-    build_array(pb_size, "new_cson", &new_cson);
+    p_array old_density = build_array(pb_size, "old_density");
+    p_array old_specific_volume = build_array(pb_size, "old_specific_volume");
+    p_array new_density = build_array(pb_size, "new_density");
+    p_array new_specific_volume = build_array(pb_size, "new_specific_volume");
+    p_array pressure = build_array(pb_size, "pressure");
+    p_array internal_energy = build_array(pb_size, "internal_energy");
+    p_array solution = build_array(pb_size, "solution");
+    p_array new_pressure = build_array(pb_size, "new_pressure");
+    p_array new_cson = build_array(pb_size, "new_cson");
 
-    fill_array(&old_density, 8230.);
-    fill_array(&new_density, 9500.);
-    fill_array(&pressure, 10.e+09);
-    fill_array(&internal_energy, 1.325e+04);
+    fill_array(old_density, 8230.);
+    fill_array(new_density, 9500.);
+    fill_array(pressure, 10.e+09);
+    fill_array(internal_energy, 1.325e+04);
 
     for (size_t i = 0; i < pb_size; ++i) {
-        old_specific_volume.data[i] = 1. / old_density.data[i];
-        new_specific_volume.data[i] = 1. / new_density.data[i];
+        old_specific_volume->data[i] = 1. / old_density->data[i];
+        new_specific_volume->data[i] = 1. / new_density->data[i];
     }
 
-    launch_vnr_resolution(&old_specific_volume, &new_specific_volume, &pressure, &internal_energy, &solution, &new_pressure, &new_cson);
+    launch_vnr_resolution(old_specific_volume, new_specific_volume, pressure, internal_energy,
+                          solution, new_pressure, new_cson);
 
     bool success = true;
-    if (!check_uniform_value(&old_density, 8230.))
+    if (!check_uniform_value(old_density, 8230.))
         success = false;
-    if (!check_uniform_value(&new_density, 9500.))
+    if (!check_uniform_value(new_density, 9500.))
         success = false;
-    if (!check_uniform_value(&pressure, 10.e+09))
+    if (!check_uniform_value(pressure, 10.e+09))
         success = false;
-    if (!check_uniform_value(&internal_energy, 1.325e+04))
+    if (!check_uniform_value(internal_energy, 1.325e+04))
         success = false;
-    if (!check_uniform_value(&solution, 200765.8953965593))
+    if (!check_uniform_value(solution, 200765.8953965593))
         success = false;
-    if (!check_uniform_value(&new_pressure, 13088079183.59054))
+    if (!check_uniform_value(new_pressure, 13088079183.59054))
         success = false;
-    if (!check_uniform_value(&new_cson, 4503.84710590959))
+    if (!check_uniform_value(new_cson, 4503.84710590959))
         success = false;
 
-    clear_array(&old_density);
-    clear_array(&old_specific_volume);
-    clear_array(&new_density);
-    clear_array(&new_specific_volume);
-    clear_array(&pressure);
-    clear_array(&internal_energy);
-    clear_array(&solution);
-    clear_array(&new_pressure);
-    clear_array(&new_cson);
+    clear_array(old_density);
+    free(old_density);
+    clear_array(old_specific_volume);
+    free(old_specific_volume);
+    clear_array(new_density);
+    free(new_density);
+    clear_array(new_specific_volume);
+    free(new_specific_volume);
+    clear_array(pressure);
+    free(pressure);
+    clear_array(internal_energy);
+    free(internal_energy);
+    clear_array(solution);
+    free(solution);
+    clear_array(new_pressure);
+    free(new_pressure);
+    clear_array(new_cson);
+    free(new_cson);
+
 
     if (!success)
         return (EXIT_FAILURE);
--- Newton/src/test_solver_perfs.c
+++ Newton/src/test_solver_perfs.c
@@ -21,58 +21,56 @@ int main()
     const size_t pb_size = 10000;
     const int nbr_of_cycles = 50000;
 
-    s_array solution, old_density, old_specific_volume;
-    s_array new_density, new_specific_volume, pressure;
-    s_array internal_energy, new_pressure, new_cson;
+    p_array old_density = build_array(pb_size, "old_density");
+    p_array old_specific_volume = build_array(pb_size, "old_specific_volume");
+    p_array new_density = build_array(pb_size, "new_density");
+    p_array new_specific_volume = build_array(pb_size, "new_specific_volume");
+    p_array pressure = build_array(pb_size, "pressure");
+    p_array internal_energy = build_array(pb_size, "internal_energy");
+    p_array solution = build_array(pb_size, "solution");
+    p_array new_pressure = build_array(pb_size, "new_pressure");
+    p_array new_cson = build_array(pb_size, "new_cson");
 
-    build_array(pb_size, "old_density", &old_density);
-    build_array(pb_size, "old_specific_volume", &old_specific_volume);
-    build_array(pb_size, "new_density", &new_density);
-    build_array(pb_size, "new_specific_volume", &new_specific_volume);
-    build_array(pb_size, "pressure", &pressure);
-    build_array(pb_size, "internal_energy", &internal_energy);
-    build_array(pb_size, "solution", &solution);
-    build_array(pb_size, "new_pressure", &new_pressure);
-    build_array(pb_size, "new_cson", &new_cson);
-
-    fill_array(&old_density, 8230.);
-    fill_array(&new_density, 9500.);
-    fill_array(&pressure, 10.e+09);
-    fill_array(&internal_energy, 1.325e+04);
+    fill_array(old_density, 8230.);
+    fill_array(new_density, 9500.);
+    fill_array(pressure, 10.e+09);
+    fill_array(internal_energy, 1.325e+04);
 
     for (size_t i = 0; i < pb_size; ++i) {
-        old_specific_volume.data[i] = 1. / old_density.data[i];
-        new_specific_volume.data[i] = 1. / new_density.data[i];
+        old_specific_volume->data[i] = 1. / old_density->data[i];
+        new_specific_volume->data[i] = 1. / new_density->data[i];
     }
     //
     start = clock();
     for (int cycle = 0; cycle < nbr_of_cycles; ++cycle)
     {
-        launch_vnr_resolution(&old_specific_volume, &new_specific_volume, &pressure, &internal_energy, &solution, &new_pressure, &new_cson);
+        launch_vnr_resolution(old_specific_volume, new_specific_volume, pressure, internal_energy,
+                              solution, new_pressure, new_cson);
         if (cycle % 1000 == 0)
         {
             srand48(time(NULL));
             int i = (int)(drand48() * pb_size);
-            printf("Cycle %d ==> Energie Interne[%d] = %15.9g | Pression[%d] = %15.9g | Vitesse du son[%d] = %15.9g\n", cycle, i, solution.data[i], i, new_pressure.data[i], i, new_cson.data[i]);
+            printf("Cycle %d ==> Energie Interne[%d] = %15.9g | Pression[%d] = %15.9g | Vitesse du son[%d] = %15.9g\n",
+                   cycle, i, solution->data[i], i, new_pressure->data[i], i, new_cson->data[i]);
         };
     }
     end = clock();
     double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
     bool success = true;
-    if (!check_uniform_value(&old_density, 8230.))
+    if (!check_uniform_value(old_density, 8230.))
         success = false;
-    if (!check_uniform_value(&new_density, 9500.))
+    if (!check_uniform_value(new_density, 9500.))
         success = false;
-    if (!check_uniform_value(&pressure, 10.e+09))
+    if (!check_uniform_value(pressure, 10.e+09))
         success = false;
-    if (!check_uniform_value(&internal_energy, 1.325e+04))
+    if (!check_uniform_value(internal_energy, 1.325e+04))
         success = false;
-    if (!check_uniform_value(&solution, 200765.8953965593))
+    if (!check_uniform_value(solution, 200765.8953965593))
         success = false;
-    if (!check_uniform_value(&new_pressure, 13088079183.59054))
+    if (!check_uniform_value(new_pressure, 13088079183.59054))
         success = false;
-    if (!check_uniform_value(&new_cson, 4503.84710590959))
+    if (!check_uniform_value(new_cson, 4503.84710590959))
         success = false;
     // At the time this test has been created, it tooks around 3 minutes to run.
     // Adds a possible variation of 10%
@@ -83,15 +81,24 @@ int main()
         printf("CPU time used (%6.4g seconds) is above the limit (%6.4g seconds)!\n", cpu_time_used, time_limit);
     }
 
-    clear_array(&old_density);
-    clear_array(&old_specific_volume);
-    clear_array(&new_density);
-    clear_array(&new_specific_volume);
-    clear_array(&pressure);
-    clear_array(&internal_energy);
-    clear_array(&solution);
-    clear_array(&new_pressure);
-    clear_array(&new_cson);
+    clear_array(old_density);
+    free(old_density);
+    clear_array(old_specific_volume);
+    free(old_specific_volume);
+    clear_array(new_density);
+    free(new_density);
+    clear_array(new_specific_volume);
+    free(new_specific_volume);
+    clear_array(pressure);
+    free(pressure);
+    clear_array(internal_energy);
+    free(internal_energy);
+    clear_array(solution);
+    free(solution);
+    clear_array(new_pressure);
+    free(new_pressure);
+    clear_array(new_cson);
+    free(new_cson);
 
     if (!success)
         return (EXIT_FAILURE);
