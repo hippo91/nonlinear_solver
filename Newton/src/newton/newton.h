b37301d77700d69a337d2f781254b1c7aa59bbe9
hippo91 <guillaume.peillex@gmail.com>
2020-04-23 15:43:29 +0200

    Simplifies the declaration of the NewtonParamters structure

--- Newton/src/newton/newton.h
+++ Newton/src/newton/newton.h
@@ -1,19 +1,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-typedef void ComputeFandDerivative(void *, double *, size_t, double *, double *);
-
-typedef void IncrementVector(double *, double *, double *, size_t, double *);
-
-typedef void CheckCriterion(double *, double *, double *, size_t, double *, bool *);
-
-typedef struct NewtonParameters NewtonParameters_t;
-
-struct NewtonParameters
+typedef struct NewtonParameters
 {
-    ComputeFandDerivative *evaluate_the_function;
-    IncrementVector *increment_the_vector;
-    CheckCriterion *check_convergence;
-};
+    void (*evaluate_the_function)(void *, double *, size_t, double *, double *);
+    void (*increment_the_vector)(double *, double *, double *, size_t, double *);
+    void (*check_convergence)(double *, double *, double *, size_t, double *, bool *);
+} NewtonParameters_t;
 
 void solveNewton(NewtonParameters_t *, void *, double *, size_t, double *);
