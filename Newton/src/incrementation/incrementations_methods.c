05a3a4a73ab050b0e0a591416b8fe80e72751289
hippo91 <guillaume.peillex@gmail.com>
2020-04-25 11:23:40 +0200

    Adds assertion before running the computation

--- Newton/src/incrementation/incrementations_methods.c
+++ Newton/src/incrementation/incrementations_methods.c
@@ -1,8 +1,15 @@
+#include <assert.h>
 #include <stdlib.h>
 #include "array.h"
 
 void classical_incrementation(__attribute__((unused)) const p_array x_k, const  p_array func, const p_array dfunc, p_array vector_of_increments)
 {
+    assert(is_valid_array(func));
+    assert(is_valid_array(dfunc));
+    assert(is_valid_array(vector_of_increments));
+    assert(func->size == dfunc->size);
+    assert(func->size == vector_of_increments->size);
+
     for (size_t i = 0; i < func->size; ++i)
     {
         vector_of_increments->data[i] = -func->data[i] / dfunc->data[i];
@@ -11,6 +18,12 @@ void classical_incrementation(__attribute__((unused)) const p_array x_k, const  
 
 void damped_incrementation(__attribute__((unused)) const p_array x_k, const p_array func, const p_array dfunc, p_array vector_of_increments)
 {
+    assert(is_valid_array(func));
+    assert(is_valid_array(dfunc));
+    assert(is_valid_array(vector_of_increments));
+    assert(func->size == dfunc->size);
+    assert(func->size == vector_of_increments->size);
+
     const double damping_coeff = 0.5;
     for (size_t i = 0; i < func->size; ++i)
     {
@@ -20,6 +33,14 @@ void damped_incrementation(__attribute__((unused)) const p_array x_k, const p_ar
 
 void ensure_same_sign_incrementation(const p_array x_k, const p_array func, const p_array dfunc, p_array vector_of_increments)
 {
+    assert(is_valid_array(x_k));
+    assert(is_valid_array(func));
+    assert(is_valid_array(dfunc));
+    assert(is_valid_array(vector_of_increments));
+    assert(func->size == x_k->size);
+    assert(func->size == dfunc->size);
+    assert(func->size == vector_of_increments->size);
+
     for (size_t i = 0; i < func->size; ++i)
     {
         double min_authorized = 0.;
