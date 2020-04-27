78127e57998fe3c222144a8b053744bdf649ef3a
hippo91 <guillaume.peillex@gmail.com>
2020-04-27 18:21:26 +0200

    Tries to build the extension using CMake

--- setup.py
+++ setup.py
@@ -1,35 +1,95 @@
-#!/usr/bin/env python
-
 """
-setup.py file for vNR scheme internal energy evolution swig project
+This module build the module vnr_internal_energy that solves the evolution
+of internal energy in the Von-Neumann Richtmeyer scheme
+
+Modified version of https://github.com/m-pilia/disptools
 """
+import os
+import platform
+import subprocess
+import sys
+import sysconfig
+from pprint import pprint
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
-#from distutils.core import setup, Extension
-from distutils.core import *
-from distutils      import sysconfig
 
-# Third-party modules - we depend on numpy for everything
-import numpy
+class CMakeExtension(Extension):
+    def __init__(self, name, cmake_lists_dir='.', sources=[], **kwa):
+        Extension.__init__(self, name, sources=sources, **kwa)
+        self.cmake_lists_dir = os.path.abspath(cmake_lists_dir)
 
-# Obtain the numpy include directory.  This logic works across numpy versions.
-try:
-    numpy_include = numpy.get_include()
-except AttributeError:
-    numpy_include = numpy.get_numpy_include()
 
-vnrinternalenergy_module = Extension('_vnrinternalenergy',
-                           sources=['launch_vnr_resolution_wrap.c', 'incrementations_methods.c', 'launch_vnr_resolution.c',
-                                    'miegruneisen.c', 'newton.c', 'stop_criterions.c', 'utils.c',
-                                    'vnr_internalenergy_evolution.c'],
-                           include_dirs = [numpy_include],
-                           extra_compile_args=['-O3', '-march=native', '-mtune=native', '-std=gnu99', '-fopenmp'],
-                           extra_link_args=['-fopenmp'],
-                           )
+class CMakeBuild(build_ext):
 
-setup (name = 'vnrinternalenergy',
-       version = '0.1',
-       author      = "hippo91",
-       description = """This module compute the evolution of the internal energy in the vNR scheme for a MieGruneisen eos""",
-       ext_modules = [vnrinternalenergy_module],
-       py_modules = ["vnrinternalenergy"],
-       )
+    def build_extensions(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError('Cannot find CMake executable')
+
+        for ext in self.extensions:
+
+            extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+            cfg = os.environ.get('Debug', 'Release')
+
+            cmake_args = [
+                '-DCMAKE_BUILD_TYPE=%s' % cfg,
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
+                '-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), self.build_temp),
+                '-DPYTHON_LIBRARY_NAME={}'.format(ext.name + sysconfig.get_config_var('SO'))
+            ]
+
+            if platform.system() == 'Windows':
+                plat = ('x64' if platform.architecture()[0] == '64bit' else 'Win32')
+                cmake_args += [
+                    '-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE',
+                    '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
+                ]
+                if self.compiler.compiler_type == 'msvc':
+                    cmake_args += [
+                        '-DCMAKE_GENERATOR_PLATFORM=%s' % plat,
+                    ]
+                else:
+                    cmake_args += [
+                        '-G', 'MinGW Makefiles',
+                    ]
+
+            pprint(cmake_args)
+
+            if not os.path.exists(self.build_lib):
+                os.makedirs(self.build_lib)
+            if not os.path.exists(self.build_temp):
+                os.makedirs(self.build_temp)
+
+            # Config and build the extension
+            subprocess.check_call(['cmake', ext.cmake_lists_dir] + cmake_args,
+                                  cwd=self.build_temp)
+            subprocess.check_call(['cmake', '--build', '.', '--config', cfg],
+                                  cwd=self.build_temp)
+
+
+# The following line is parsed by Sphinx
+version = '1.0.0'
+
+setup(name='vnr_internal_energy',
+      version=version,
+      description='Compute internal energy evolution in the Von-Neumann Richtmeyer scheme',
+      author='Guillaume Peillex',
+      author_email='guillaume.peillex@gmail.com',
+      url='https://github.com/hippo91/nonlinear_solver',
+      keywords=['hydrocode', 'vnr', 'equation of state'],
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      install_requires=['numpy>=1.15.1'],
+      ext_modules=[CMakeExtension('_vnr_internal_energy')],
+      cmdclass={'build_ext': CMakeBuild},
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: MacOS",
+          "Operating System :: Microsoft :: Windows",
+          "Operating System :: POSIX :: Linux",
+      ],
+      )
