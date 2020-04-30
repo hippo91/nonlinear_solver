5ed63864484ef0aa554d3f6ffb4ad363784ec13c
hippo91 <guillaume.peillex@gmail.com>
2020-04-30 12:44:38 +0200

    Adds asserts

--- src/functions/cubic.c
+++ src/functions/cubic.c
@@ -1,8 +1,17 @@
 #include "cubic.h"
+#include <assert.h>
+
 #include "array.h"
+
 
 void cubic_function(__attribute__((unused)) void *params, const p_array x, p_array fx, p_array dfx)
 {
+    assert(is_valid_array(x));
+    assert(is_valid_array(fx));
+    assert(is_valid_array(dfx));
+    assert(x->size == fx->size);
+    assert(x->size == dfx->size);
+
     for (unsigned int i = 0; i < x->size; ++i)
     {
         fx->data[i] = x->data[i] * x->data[i] * x->data[i] - 2. * x->data[i] * x->data[i] + 1;
