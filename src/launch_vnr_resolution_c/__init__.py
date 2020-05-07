839ce5b298b2cdfd4f22d7c8fe4520202ad77370
hippo91 <guillaume.peillex@gmail.com>
2020-05-07 12:05:47 +0200

    Creates a wrapper class to ease the init of the MieGruneisenParams_s class

--- src/launch_vnr_resolution_c/__init__.py
+++ src/launch_vnr_resolution_c/__init__.py
@@ -1,1 +1,16 @@
 from .vnr_internal_energy import launch_vnr_resolution, MieGruneisenParams_s
+
+class MieGruneisenParams(MieGruneisenParams_s):
+    """
+    Just a wrapper class around MieGruneisenParams_s
+    """
+    def __init__(self, czero, b, ezero, grunzero, rhozero, S1, S2, S3):
+        super().__init__()
+        self.c_zero = czero
+        self.coeff_b = b
+        self.e_zero = ezero
+        self.gamma_zero = grunzero
+        self.rho_zero = rhozero
+        self.s1 = S1
+        self.s2 = S2
+        self.s3 = S3
